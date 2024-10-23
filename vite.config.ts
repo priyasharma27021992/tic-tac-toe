e933b6758ab215f0f57f435951e2c8feb9471aa0
Priya.Sharma <priyasharma2702@gmail.com>
2024-10-23 23:25:28 +0530

    trying to get deployment work

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tic-tac-toe", // YOUR REPO NAME HERE
+  base: "/tic-tac-toe/", // YOUR REPO NAME HERE
   plugins: [react()],
 });
